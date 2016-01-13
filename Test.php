ba0624585e2d8974cbe784be2ff504dd84a81471
Domenic Rosati <domenicrosati@gmail.com>
2016-01-13 15:01:21 -0400

    put in some php tests

--- Test.php
+++ Test.php
@@ -1,1 +1,14 @@
-<? PHP echo("hi"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>php test</title>
+</head>  
+<body>
+    <?php 
+      echo("hi \n");
+      print("hello \n");
+      printf("and hi \n")
+    ?>
+</body>
+</html>
+
